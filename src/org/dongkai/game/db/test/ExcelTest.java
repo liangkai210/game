a2052d47da94eaa0ca590cea2975d62122508bda
zhou-dong <82224165@qq.com>
2016-02-17 22:10:46 -0600

    update

--- src/org/dongkai/game/db/test/ExcelTest.java
+++ src/org/dongkai/game/db/test/ExcelTest.java
@@ -7,13 +7,12 @@ import org.dongkai.game.load.ExcelRead;
 public class ExcelTest {
 
 	public static void testRead() throws IOException {
-
 		ExcelRead read = new ExcelRead();
-
 		read.read();
 	}
 
 	public static void main(String[] args) throws IOException {
 		testRead();
 	}
+
 }
--- src/org/dongkai/game/load/ExcelRead.java
+++ src/org/dongkai/game/load/ExcelRead.java
@@ -1,8 +1,5 @@
 package org.dongkai.game.load;
 
-import java.io.Closeable;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.logging.Logger;
@@ -10,53 +7,28 @@ import java.util.logging.Logger;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.dongkai.game.db.DataBase;
 
 public class ExcelRead {
 
 	static Logger logger = Logger.getLogger(ExcelRead.class.getName());
 
-	private String path = "/Users/dongdong/Downloads/Chem_Chem_Structure_Score.xlsx";
+	// private String path =
+	// "/Users/dongdong/Downloads/Chem_Chem_Structure_Score.xlsx";
 
 	public void read() throws IOException {
-
-		FileInputStream file = new FileInputStream(new File(path));
-
-		// Get the workbook instance for XLS file
-		XSSFWorkbook workbook = new XSSFWorkbook(file);
-
-		// Get first sheet from the workbook
-		XSSFSheet sheet = workbook.getSheetAt(0);
-
-		// Get iterator to all the rows in current sheet
+		XSSFSheet sheet = DataBase.EXCEL.getFirstXSSFSheet();
 		Iterator<Row> rowIterator = sheet.iterator();
-
-		// Get iterator to all cells of current row
 		while (rowIterator.hasNext()) {
 			Row row = rowIterator.next();
-
-			System.out.println(row);
-
 			Iterator<Cell> cellIterator = row.cellIterator();
 			while (cellIterator.hasNext()) {
 				Cell cell = cellIterator.next();
-				System.out.print(cell.getStringCellValue());
+				System.out.print(cell.getNumericCellValue() + " ");
 			}
 			System.out.println();
 		}
-		close(workbook);
-		close(file);
-	}
-
-	public void close(Closeable close) {
-		if (close == null) {
-			return;
-		}
-		try {
-			close.close();
-		} catch (IOException e) {
-			logger.info(e.getMessage());
-		}
+		DataBase.EXCEL.closeWorkBook();
 	}
 
 }
